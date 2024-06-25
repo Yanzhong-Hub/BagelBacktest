b8dfd337fbbbca6b4a7ca655c51d62f69b949e07
Eric Huang <eric.yanzhong.huang@outlook.com>
2024-06-25 11:38:52 +0800

    Add Portfolio and Core files to BagelBacktest
    This commit introduces two new classes, Portfolio and Core, to the BagelBacktest module. The Portfolio class will be used to contain backtest results, while the Core class serves as the user interface for the BagelBacktest package. Still need to add core calculation and process

---
+++ BagelBacktest/portfolio.py
@@ -0,0 +1,16 @@
+"""
+Portfolio class
+Author: Eric Yanzhong Huang
+Email: eric.yanzhong.huang@outlook.com
+"""
+
+from dataclasses import dataclass
+
+
+@dataclass(slots=True)
+class Portfolio:
+    """
+    Portfolio class
+    contains backtest results
+    """
+    pass
